080719fac3657c7c17ea28983adada3d02a86d22
MolchanovSergii <molchanse@gmail.com>
2023-08-04 11:11:14 +0300

    start task

---
+++ index.js
@@ -0,0 +1,25 @@
+const fs = require("fs/promises");
+const argv = require("yargs").argv;
+
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      // ...
+      break;
+
+    case "get":
+      // ... id
+      break;
+
+    case "add":
+      // ... name email phone
+      break;
+
+    case "remove":
+      // ... id
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
